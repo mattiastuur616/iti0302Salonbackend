bd11292e6368db43de64815535f66ba6ac893628
mattiastuur616 <mattiastuur@gmail.com>
2023-06-30 14:37:38 +0300

    Adding functions with the cosmetics.

--- src/main/java/ee/taltech/iti03022023salonbackend/controller/SystemController.java
+++ src/main/java/ee/taltech/iti03022023salonbackend/controller/SystemController.java
@@ -1,7 +1,9 @@
 package ee.taltech.iti03022023salonbackend.controller;
 
 import ee.taltech.iti03022023salonbackend.dto.ClientDto;
+import ee.taltech.iti03022023salonbackend.dto.CosmeticDto;
 import ee.taltech.iti03022023salonbackend.model.Client;
+import ee.taltech.iti03022023salonbackend.model.Cosmetic;
 import ee.taltech.iti03022023salonbackend.service.SystemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,18 +15,67 @@ import java.util.List;
 public class SystemController {
     private final SystemService systemService;
 
+    /**
+     * Get request for showing all the clients in the salon system.
+     *
+     * @return the list of clients
+     */
     @GetMapping("/allClients")
     public List<ClientDto> getAllClients() {
         return systemService.getAllClients();
     }
 
+    /**
+     * Post request for adding a new client to the salon.
+     *
+     * @param client to be added
+     * @return the string explaining the result
+     */
     @PostMapping("/addClient")
     public String addClient(@RequestBody Client client) {
         return systemService.addClient(client);
     }
 
+    /**
+     * Delete request for removing the client from the salon.
+     *
+     * @param id of the client to be removed
+     * @return the string explaining the result
+     */
     @DeleteMapping("/removeClient/{id}")
     public String removeClient(@PathVariable Long id) {
         return systemService.removeClient(id);
+    }
+
+    /**
+     * Get request for showing all the cosmetics in the salon system.
+     *
+     * @return the list of cosmetics
+     */
+    @GetMapping("/allCosmetics")
+    public List<CosmeticDto> getAllCosmetics() {
+        return systemService.getAllCosmetics();
+    }
+
+    /**
+     * Post request for adding a new cosmetic to the salon.
+     *
+     * @param cosmetic to be added
+     * @return the string explaining the result
+     */
+    @PostMapping("/addCosmetic")
+    public String addCosmetic(@RequestBody Cosmetic cosmetic) {
+        return systemService.addCosmetic(cosmetic);
+    }
+
+    /**
+     * Delete request for removing the cosmetic from the salon.
+     *
+     * @param id of the cosmetic to be removed
+     * @return the string explaining the result
+     */
+    @DeleteMapping("/removeCosmetic/{id}")
+    public String removeCosmetic(@PathVariable Long id) {
+        return systemService.removeCosmetic(id);
     }
 }
--- src/main/java/ee/taltech/iti03022023salonbackend/service/SystemService.java
+++ src/main/java/ee/taltech/iti03022023salonbackend/service/SystemService.java
@@ -1,7 +1,9 @@
 package ee.taltech.iti03022023salonbackend.service;
 
 import ee.taltech.iti03022023salonbackend.dto.ClientDto;
+import ee.taltech.iti03022023salonbackend.dto.CosmeticDto;
 import ee.taltech.iti03022023salonbackend.model.Client;
+import ee.taltech.iti03022023salonbackend.model.Cosmetic;
 import ee.taltech.iti03022023salonbackend.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,14 @@ public class SystemService {
     private final ServiceStatusRepository serviceStatusRepository;
     private final ServiceTypeRepository serviceTypeRepository;
 
+
+    // Transactions with the clients.
+
+    /**
+     * Method for showing all the clients existing in the database.
+     *
+     * @return the list of clients
+     */
     public List<ClientDto> getAllClients() {
         List<ClientDto> clientDtoList = new ArrayList<>();
         for (Client client : clientRepository.findAll()) {
@@ -28,6 +38,14 @@ public class SystemService {
         return clientDtoList;
     }
 
+    /**
+     * Method for adding a new client to the database.
+     * Because of every client has to have their unique email address, function will check
+     * if the database doesn't already have a client with the same email.
+     *
+     * @param client to be added
+     * @return the string explaining the result
+     */
     public String addClient(Client client) {
         Optional<Client> existingClient = clientRepository.findClientsByEmailIgnoreCase(client.getEmail());
         if (existingClient.isPresent()) {
@@ -37,6 +55,13 @@ public class SystemService {
         return "New client " + client.getFirstName() + " " + client.getLastName() + " was successfully added.";
     }
 
+    /**
+     * Method for removing the client from the database.
+     * Checks if the client even exists before trying to remove them.
+     *
+     * @param id of the client to be removed
+     * @return the string explaining the result
+     */
     public String removeClient(Long id) {
         Optional<Client> existingClient = clientRepository.findById(id);
         if (existingClient.isEmpty()) {
@@ -47,9 +72,78 @@ public class SystemService {
         return "Client " + client.getFirstName() + " " + client.getLastName() + " was removed from the database.";
     }
 
+    /**
+     * Help function to convert original client object into the data transfer object.
+     *
+     * @param client to be converted
+     * @return dto of the original client object
+     */
     public ClientDto convertIntoClientDto(Client client) {
         return new ClientDto(client.getClientId(), client.getFirstName(), client.getLastName(), client.getMoney(),
                 client.getPhoneNumber(), client.getEmail(), client.getIdCode(), client.getDateOfBirth(),
                 client.getHomeAddress());
+    }
+
+
+    // Transactions with the cosmetics.
+
+    /**
+     * Method for showing all the cosmetics existing in the database.
+     *
+     * @return the list of cosmetics
+     */
+    public List<CosmeticDto> getAllCosmetics() {
+        List<CosmeticDto> cosmeticDtoList = new ArrayList<>();
+        for (Cosmetic cosmetic : cosmeticRepository.findAll()) {
+            cosmeticDtoList.add(convertIntoCosmeticDto(cosmetic));
+        }
+        return cosmeticDtoList;
+    }
+
+    /**
+     * Method for adding a new cosmetic into the database.
+     * Because of each one of the cosmetic needs to own unique email address the function check
+     * if there isn't already existing cosmetic with that email.
+     *
+     * @param cosmetic to be added
+     * @return the string explaining the result
+     */
+    public String addCosmetic(Cosmetic cosmetic) {
+        Optional<Cosmetic> existingCosmetic = cosmeticRepository.findCosmeticsByEmailIgnoreCase(cosmetic.getEmail());
+        if (existingCosmetic.isPresent()) {
+            return "Cosmetic with this email address already existing in the database";
+        }
+        cosmeticRepository.save(cosmetic);
+        return "New cosmetic " + cosmetic.getFirstName() + " " + cosmetic.getLastName() + " was added into " +
+                "the database.";
+    }
+
+    /**
+     * Method for removing the cosmetic from the database.
+     * The function checks if the cosmetic even exists before trying to remove them.
+     *
+     * @param id of the cosmetic to be removed
+     * @return the string explaining the result
+     */
+    public String removeCosmetic(Long id) {
+        Optional<Cosmetic> existingCosmetic = cosmeticRepository.findById(id);
+        if (existingCosmetic.isEmpty()) {
+            return "No such cosmetic in the database.";
+        }
+        Cosmetic cosmetic = existingCosmetic.get();
+        cosmeticRepository.delete(cosmetic);
+        return "Cosmetic " + cosmetic.getFirstName() + " " + cosmetic.getLastName() + " was removed from " +
+                "the database.";
+    }
+
+    /**
+     * Help function to convert the original object into the data transfer object.
+     *
+     * @param cosmetic to be converted
+     * @return dto of the original cosmetic object
+     */
+    public CosmeticDto convertIntoCosmeticDto(Cosmetic cosmetic) {
+        return new CosmeticDto(cosmetic.getCosmeticId(), cosmetic.getFirstName(), cosmetic.getLastName(),
+                cosmetic.getPhoneNumber(), cosmetic.getEmail());
     }
 }
